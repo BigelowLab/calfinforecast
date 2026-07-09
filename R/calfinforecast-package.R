9fd7336cb79d6619b2af8ec244e0077d955ae2d2
Ben Tupper <btupper@bigelow.org>
2026-07-09 08:17:19 -0400

    auto update 2026-07-09 08:17:19

--- R/calfinforecast-package.R
+++ R/calfinforecast-package.R
@@ -4,4 +4,5 @@
 #' @aliases calfinforecast
 #' @description Serves a subset of ecopmo calfin predictive maps
 #' @keywords internal
+#' @import sf stars
 "_PACKAGE"
--- R/graphics.R
+++ R/graphics.R
@@ -9,21 +9,25 @@
 #' @return either a single ggplot object (facet wrapped by time) or 
 #'   a list of ggplot objects (one per unit of time)
 plot_forecast = function(x = read_raster(),
-                         wrap = TRUE,
+                         wrap = length(dim(x)) > 2,
                          crop = NULL){
   coastline = read_coastline()
-  d = dim(x)
-  if (length(d) == 2 || d[3] == 1) wrap = FALSE
-  time = stars::st_get_dimension_values(s, "time")
+ 
   
   if (!is.null(crop)){
     coastline = sf::st_crop(coastline, crop)
-    s = sf::st_crop(s, crop)
+    x = st_crop(x, crop)
+  }
+  
+  if (length(dim(x)) == 2){
+    time = as.Date("1776-07-02")
+  } else {
+    time = stars::st_get_dimension_values(x, "time")
   }
   
   if (wrap){
     gg = ggplot2::ggplot() +
-      stars::geom_stars(data = s,
+      stars::geom_stars(data = x,
                         na.action = na.omit) +
       viridis::scale_fill_viridis(limits = c(0,1)) + 
       ggplot2::geom_sf(data = coastline, color = "orange") + 
@@ -33,7 +37,7 @@ plot_forecast = function(x = read_raster(),
     gg = lapply(seq_along(time),
                 function(i){
                   ggplot2::ggplot() +
-                    stars::geom_stars(data = dplyr::slice(s, "time", i),
+                    stars::geom_stars(data = dplyr::slice(x, "time", i),
                                       na.action = na.omit) +
                     viridis::scale_fill_viridis(limits = c(0,1)) + 
                     ggplot2::geom_sf(data = coastline, color = "orange") +  
